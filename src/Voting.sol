4b8e2c3a04fa489c63b44843f239e014c99da56c
Denis <denis.saltykov.12@mail.ru>
2025-07-08 14:00:12 +0300

    feat: change VoteSession struct, edit winners type

--- src/Voting.sol
+++ src/Voting.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.30;
 
 contract Voting {
-
-
     error NameVoteSessionCantBeEmpty();
     error StartTimeMoreOrEqualEndTime();
     error StartTimeLessTimestamp();
@@ -21,14 +19,27 @@ contract Voting {
     error UserNotVoterInThisVoteSession(uint256 idVoteSession);
     error VoteSessionAlreadyEnded();
 
-    event VoteSessionCreated(uint256 voteSessionId,string name,uint256 startTime, uint256 endTime);
+    event VoteSessionCreated(uint256 voteSessionId, string name, uint256 startTime, uint256 endTime);
     event Voted(uint256 voteSessionId, address voter, string choice);
-    event VoteSessionEnded(uint256 voteSessionId,uint256 countVoters, StatusVoteSession status,string winner);
+    event VoteSessionEnded(uint256 voteSessionId, uint256 countVoters, StatusVoteSession status, string[] winners);
 
-    enum StatusVoteSession{Created, Active, Ended, Rejected}
-    enum VoteAccess{Public, Yes, No}
-    enum VoteChoice{Choice1, Choice2, Choice3, Choice4}
-
+    enum StatusVoteSession {
+        Created,
+        Active,
+        Ended,
+        Rejected
+    }
+    enum VoteAccess {
+        Public,
+        Yes,
+        No
+    }
+    enum VoteChoice {
+        Choice1,
+        Choice2,
+        Choice3,
+        Choice4
+    }
 
     struct Choice {
         string title;
@@ -55,7 +66,7 @@ contract Voting {
         mapping(address => Voter) voters;
         StatusVoteSession status;
         Choice[] choices;
-        Choice[] winners;
+        string[] winners;
     }
 
     uint256 public countVoteSessions;
@@ -71,100 +82,114 @@ contract Voting {
         bool _isPrivate,
         Voter[] memory _voters,
         string[] calldata _choices
-        ) external {
-            if (bytes(_title).length == 0) revert NameVoteSessionCantBeEmpty();
-            if (_startTime >= _endTime) revert StartTimeMoreOrEqualEndTime();
-            if (_startTime < block.timestamp) revert StartTimeLessTimestamp();
-            if (_endTime < block.timestamp) revert EndTimeLessTimestamp();
-            if (_choices.length == 0) revert CountChoosesCantBeZero();
-            if (_choices.length > MAX_COUNT_CHOICES) revert CountChoosesMoreFour();
-            if (_isPrivate) {
-                for (uint256 i = 0; i < _voters.length; i++) {
-                    if (_voters[i].addr == address(0)) revert VoterAddressCantBeZero();
-                    _voters[i].canVote = VoteAccess.Yes;
+    ) external {
+        if (bytes(_title).length == 0) revert NameVoteSessionCantBeEmpty();
+        if (_startTime >= _endTime) revert StartTimeMoreOrEqualEndTime();
+        if (_startTime < block.timestamp) revert StartTimeLessTimestamp();
+        if (_endTime < block.timestamp) revert EndTimeLessTimestamp();
+        if (_choices.length == 0) revert CountChoosesCantBeZero();
+        if (_choices.length > MAX_COUNT_CHOICES) revert CountChoosesMoreFour();
+        if (_isPrivate) {
+            for (uint256 i = 0; i < _voters.length; i++) {
+                if (_voters[i].addr == address(0)) {
+                    revert VoterAddressCantBeZero();
+                }
+                _voters[i].canVote = VoteAccess.Yes;
+            }
+        }
+        for (uint256 i = 0; i < _choices.length; i++) {
+            if (bytes(_choices[i]).length == 0) revert ChoiceLengthCantBeZero();
+        }
+        countVoteSessions++;
+        VoteSession storage voteSession = voteSessions[countVoteSessions];
+        voteSession.id = countVoteSessions;
+        voteSession.creatorAddr = msg.sender;
+        voteSession.title = _title;
+        voteSession.description = _description;
+        voteSession.startTime = _startTime;
+        voteSession.endTime = _endTime;
+        voteSession.minNumberVotes = _minNumberVotes;
+        voteSession.tempNumberVotes = 0;
+        voteSession.isPrivate = _isPrivate;
+        for (uint256 i = 0; i < _voters.length; i++) {
+            voteSession.voters[_voters[i].addr] = _voters[i];
+        }
+        for (uint256 i = 0; i < _choices.length; i++) {
+            voteSession.choices.push(Choice(_choices[i], 0));
+        }
+        voteSession.status = StatusVoteSession.Created;
+        emit VoteSessionCreated(voteSession.id, voteSession.title, voteSession.startTime, voteSession.endTime);
+    }
+
+    function vote(uint256 _voteSessionId, uint256 _indChoice) external {
+        if (_voteSessionId > countVoteSessions || _voteSessionId == 0) {
+            revert VoteSessionNotFound(_voteSessionId, countVoteSessions);
+        }
+        VoteSession storage voteSession = voteSessions[_voteSessionId];
+        uint256 countChoices = voteSession.choices.length;
+        if (_indChoice > countChoices) {
+            revert ChoiceNotFound(_voteSessionId, countChoices, _indChoice);
+        }
+        if (block.timestamp < voteSession.startTime) {
+            revert VoteSessionNotStarted(_voteSessionId, voteSession.startTime, block.timestamp);
+        }
+        if (
+            block.timestamp > voteSession.endTime || voteSession.status == StatusVoteSession.Ended
+                || voteSession.status == StatusVoteSession.Rejected
+        ) {
+            revert VoteSessionHasEnded(_voteSessionId, voteSession.endTime, block.timestamp);
+        }
+        if (voteSession.voters[msg.sender].hasVoted) {
+            revert VoterAlreadyVoted(_voteSessionId);
+        }
+        if (voteSession.isPrivate) {
+            if (
+                voteSession.voters[msg.sender].canVote == VoteAccess.Public
+                    || voteSession.voters[msg.sender].canVote == VoteAccess.No
+            ) {
+                revert UserNotVoterInThisVoteSession(_voteSessionId);
+            }
+        }
+        if (block.timestamp >= voteSession.startTime && voteSession.status == StatusVoteSession.Created) {
+            voteSession.status = StatusVoteSession.Active;
+        }
+        voteSession.voters[msg.sender].hasVoted = true;
+        voteSession.choices[_indChoice].countVotes++;
+        voteSession.tempNumberVotes++;
+        voteSession.voters[msg.sender].choice = voteSession.choices[_indChoice].title;
+
+        emit Voted(_voteSessionId, msg.sender, voteSession.voters[msg.sender].choice);
+    }
+
+    function endVoteSession(uint256 _voteSessionId) public {
+        if (_voteSessionId > countVoteSessions || _voteSessionId == 0) {
+            revert VoteSessionNotFound(_voteSessionId, countVoteSessions);
+        }
+        VoteSession storage voteSession = voteSessions[_voteSessionId];
+        uint256 countChoices = voteSession.choices.length;
+        if (block.timestamp < voteSession.endTime) {
+            revert VoteSessionEndTimeNotReached(_voteSessionId, voteSession.endTime, block.timestamp);
+        }
+        if (voteSession.status == StatusVoteSession.Ended || voteSession.status == StatusVoteSession.Rejected) {
+            revert VoteSessionAlreadyEnded();
+        }
+        if (voteSession.tempNumberVotes < voteSession.minNumberVotes) {
+            voteSession.status = StatusVoteSession.Rejected;
+        } else {
+            voteSession.status = StatusVoteSession.Ended;
+            uint256 maxCountVotes;
+            for (uint256 i = 0; i < countChoices; i++) {
+                if (voteSession.choices[i].countVotes >= maxCountVotes) {
+                    maxCountVotes = voteSession.choices[i].countVotes;
                 }
             }
-            for (uint256 i = 0;i < _choices.length; i++) {
-                if (bytes(_choices[i]).length == 0) revert ChoiceLengthCantBeZero();
+            for (uint256 i = 0; i < countChoices; i++) {
+                if (voteSession.choices[i].countVotes == maxCountVotes) {
+                    voteSession.winners.push(voteSession.choices[i].title);
+                }
             }
-            countVoteSessions++;
-            VoteSession storage voteSession = voteSessions[countVoteSessions];
-            voteSession.id = countVoteSessions;
-            voteSession.creatorAddr = msg.sender;
-            voteSession.title = _title;
-            voteSession.description = _description;
-            voteSession.startTime = _startTime;
-            voteSession.endTime = _endTime;
-            voteSession.minNumberVotes = _minNumberVotes;
-            voteSession.tempNumberVotes = 0;
-            voteSession.isPrivate =  _isPrivate;
-            for (uint256 i = 0; i < _voters.length;i++) {
-                voteSession.voters[_voters[i].addr] = _voters[i];
-            }
-            for (uint256 i = 0;i < _choices.length;i++) {
-                voteSession.choices.push(Choice(_choices[i],0));
-            }
-            voteSession.status = StatusVoteSession.Created;
-            voteSession.winner = string[];
-
-            emit VoteSessionCreated(voteSession.id, voteSession.name, voteSession.startTime, voteSession.endTime);
         }
 
-        function vote(uint256 _voteSessionId,uint256 _indChoice) external {
-            if (_voteSessionId > countVoteSessions || _voteSessionId == 0) revert VoteSessionNotFound(_voteSessionId, countVoteSessions);
-            VoteSession storage voteSession = voteSessions[_voteSessionId];
-            uint256 countChoices = voteSession.choices.length;
-            if (_indChoice > countChoices) revert ChoiceNotFound(_voteSessionId,countChoices,_indChoice);
-            if (block.timestamp < voteSession.startTime) revert VoteSessionNotStarted(_voteSessionId,voteSession.startTime,block.timestamp);
-            if (block.timestamp > voteSession.endTime || voteSession.status == StatusVoteSession.Ended || voteSession.status == StatusVoteSession.Rejected) {
-                revert VoteSessionHasEnded(_voteSessionId,voteSession.endTime,block.timestamp);
-            }
-            if (voteSession.voters[msg.sender].hasVoted) revert VoterAlreadyVoted(_voteSessionId);
-            if (voteSession.isPrivate) {
-                if (voteSession.voters[msg.sender].canVote == VoteAccess.Public || voteSession.voters[msg.sender].canVote == VoteAccess.No ) {
-                    revert UserNotVoterInThisVoteSession(_voteSessionId);
-                }
-            }
-            if (block.timestamp >= voteSession.startTime && voteSession.status == StatusVoteSession.Created) {
-                voteSession.status = StatusVoteSession.Active;
-            }
-            voteSession.voters[msg.sender].hasVoted = true;
-            voteSession.choices[_indChoice].countVotes++;
-            voteSession.tempNumberVotes++;
-            voteSession.voters[msg.sender].choice = voteSession.choices[_indChoice].name;
-
-            emit Voted(_voteSessionId,msg.sender,voteSession.voters[msg.sender].choice);
-        }
-
-        function endVoteSession(uint256 _voteSessionId) public {
-            if (_voteSessionId > countVoteSessions || _voteSessionId == 0) revert VoteSessionNotFound(_voteSessionId, countVoteSessions);
-            VoteSession storage voteSession = voteSessions[_voteSessionId];
-            uint256 countChoices = voteSession.choices.length;
-            if (block.timestamp < voteSession.endTime) revert VoteSessionEndTimeNotReached(_voteSessionId,voteSession.endTime,block.timestamp);
-            if (voteSession.status == StatusVoteSession.Ended || voteSession.status == StatusVoteSession.Rejected ) revert VoteSessionAlreadyEnded();
-            if (voteSession.tempNumberVotes < voteSession.minNumberVotes) {
-                voteSession.status = StatusVoteSession.Rejected;
-            }
-            else {
-                voteSession.status = StatusVoteSession.Ended;
-                string memory winnerVoteSession;
-                uint256 maxCountVotes;
-                uint256[] countsVotesChoices;
-                for (uint256 i = 0;i < countChoices;i++) {
-                    countsVotesChoices.push(voteSession.choices[i].countVotes);
-                    if (voteSession.choices[i].countVotes >= maxCountVotes) {
-                        maxCountVotes = voteSession.choices[i].countVotes;
-                    }
-                }
-                for (uint256 i = 0;i < countsVotesChoices.length;i++) {
-                    if (countsVotesChoices[i] == maxCountVotes) {
-                        voteSession.winners.push(voteSession.choices[i]);
-                    }
-                }
-
-            }
-
-            emit VoteSessionEnded(_voteSessionId, voteSession.tempNumberVotes, voteSession.status, voteSession.winners);
-
-        }
+        emit VoteSessionEnded(_voteSessionId, voteSession.tempNumberVotes, voteSession.status, voteSession.winners);
+    }
 }
